33e6340ed164697968a3053a46810e5eda7d8869
jmessina <j.w.messina24@gmail.com>
2025-10-26 19:58:35 -0600

    Creaated new file and updated constants.py file

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock_obj = pygame.time.Clock()
     while True :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (1,1,1))
         pygame.display.flip()
+        # End of loop - tick
+        dt = clock_obj.tick(60) / 1000
 
 
 if __name__ == "__main__":

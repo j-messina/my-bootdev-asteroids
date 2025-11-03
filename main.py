366b579ce8d7ceea392f932b442413ec5691eb97
jmessina <j.w.messina24@gmail.com>
2025-11-03 16:23:22 -0700

    Fixed some small bugs, updated the game code to now generate a black screen and draw the player triangle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -10,11 +11,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     clock_obj = pygame.time.Clock()
+    my_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (1,1,1))
+        my_player.draw(screen)
         pygame.display.flip()
         # End of loop - tick
         dt = clock_obj.tick(60) / 1000
--- player.py
+++ player.py
@@ -1,9 +1,10 @@
+import pygame
 from circleshape import *
-from constants import *
+from constants import * 
 
 class Player(CircleShape):
     def __init__(self, x, y):
-        super.__init__(x,y,PLAYER_RADIUS)
+        super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
 
     def triangle(self):
@@ -14,5 +15,5 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
     
-    def draw(screen):
+    def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)

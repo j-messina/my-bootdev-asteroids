72f00d8ab7b8ea264f242c2c980fa1904bb37cc6
jmessina <j.w.messina24@gmail.com>
2025-11-03 16:38:04 -0700

    Updated game code - ship now rotates using A & D keys

--- main.py
+++ main.py
@@ -17,10 +17,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (1,1,1))
+        my_player.update(dt)
         my_player.draw(screen)
         pygame.display.flip()
         # End of loop - tick
         dt = clock_obj.tick(60) / 1000
+
 
 
 if __name__ == "__main__":

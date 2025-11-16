b7a0649d1e14decf233bf46e88bc33f51001a261
jmessina <j.w.messina24@gmail.com>
2025-11-16 16:21:06 -0700

    Now draws the player as a triangle the screen, did some other stuff, uhhhh I already forgot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -13,12 +14,15 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    my_player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        my_player.draw(screen)
 
         dt = clock.tick(60) / 1000
         pygame.display.flip()
